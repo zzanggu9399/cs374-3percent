f1aa45e3814eda1125fd8d68c8a02e32ebc9998d
zzanggu9399 <48054186+zzanggu9399@users.noreply.github.com>
2020-06-03 21:47:55 +0900

    Add functions
    Databases, JS, Details ...

--- post.js
+++ post.js
@@ -1,3 +1,37 @@
-$( document ).ready(function() {
 
+    var firebaseConfig = {
+    apiKey: "AIzaSyAC7HlE8qFQ6kKX7vgxJpU_bkBV30CuMLg",
+    authDomain: "team3per.firebaseapp.com",
+    databaseURL: "https://team3per.firebaseio.com",
+    projectId: "team3per",
+    storageBucket: "team3per.appspot.com",
+    messagingSenderId: "124634951152",
+    appId: "1:124634951152:web:fa5b0ae1a4f37fd94c8135"
+  };
+
+  // Initialize Firebase
+  
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+$( document ).ready(function() {
+    $('.post').click(function(){
+        var title = document.getElementById("title");
+        var maintext = document.getElementById("main_text");
+        var category = document.getElementById("category");
+        if (category.value == "none"){
+            alert("should select category");
+        }
+        else{
+            var dic = {
+                Title : title.value,
+                Content : maintext.value,
+                Category : category.value
+            };
+            console.log(dic);
+            database.push(dic);
+        }
+        
+    });
 });
