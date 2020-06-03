ae13060f2eef7cc64e71004de43fc726d904b7de
sonjbin <sonjbin@kaist.ac.kr>
2020-06-03 22:48:54 +0900

    jungbin_6/3_layout complete

---
+++ tea_information.js
@@ -0,0 +1,42 @@
+tea_information = [
+    {
+        name: "Green tea",
+        src: "https://cdn.pixabay.com/photo/2018/07/10/11/37/green-tea-3528474_1280.jpg",
+        tag: []
+    },
+    {
+        name: "Black tea",
+        src: "https://cdn.pixabay.com/photo/2016/10/26/00/59/tea-1770415_1280.jpg",
+        tag: []
+    },
+    {
+        name: "Barley tea",
+        src: "https://cdn.pixabay.com/photo/2020/05/29/05/47/barley-5233734_1280.jpg",
+        tag: []
+    },
+    {
+        name: "Lemon tea",
+        src: "https://cdn.pixabay.com/photo/2015/09/12/20/19/lemon-tea-937245_1280.jpg",
+        tag: []
+    },
+    {
+        name: "Green tea",
+        src: " ",
+        tag: []
+    },
+    {
+        name: "Green tea",
+        src: " ",
+        tag: []
+    },
+    {
+        name: "Green tea",
+        src: " ",
+        tag: []
+    },
+    {
+        name: "Green tea",
+        src: " ",
+        tag: []
+    }
+]
--- tea_search.js
+++ tea_search.js
@@ -1,3 +1,22 @@
+/*tea_search.js*/
+
+var tea_list = tea_information
+
+
+
 $( document ).ready(function() {
+    var imagegrid = document.getElementById('image_grid');
+    for (i=0; i<tea_list.length;i++){
+        var div = document.createElement('div');
+        div.className = "grid";
+        var image = document.createElement('img');
+        image.src = tea_list[i].src;
+        image.alt = "Cannot Load Image";
+        image.className = "image";
+        div.appendChild(image)
+        imagegrid.appendChild(div);        
+    }
+    
 
 });
+
