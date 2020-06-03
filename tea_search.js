79b8aaf5e5b871e1bbb51ea646aa1886cef6ed3e
sonjbin <sonjbin@kaist.ac.kr>
2020-06-03 23:56:43 +0900

    jungbin_6/3_layout complete 2

--- tea_search.js
+++ tea_search.js
@@ -13,10 +13,17 @@ $( document ).ready(function() {
         image.src = tea_list[i].src;
         image.alt = "Cannot Load Image";
         image.className = "image";
+        
+        var p = document.createElement('p');
+        p.innerHTML = tea_list[i].name
+        p.style = "font-size:25px";
         div.appendChild(image)
+        div.appendChild(p);
+
         imagegrid.appendChild(div);        
     }
-    
-
 });
 
+function addFilter(filter_name){
+    alert(filter_name);
+}
