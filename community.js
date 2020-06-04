2a00443b2abae115c74c737a4dbc5381728473db
zzanggu9399 <48054186+zzanggu9399@users.noreply.github.com>
2020-06-04 11:37:57 +0900

    Add DB,Connection

--- community.js
+++ community.js
@@ -1,4 +1,36 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: "AIzaSyCzWgYskwsU4_XW0hicz0W7zwFEtvxT_lg",
+	authDomain: "cs374-20170364.firebaseapp.com",
+	databaseURL: "https://cs374-20170364.firebaseio.com",
+	projectId: "cs374-20170364",
+	storageBucket: "cs374-20170364.appspot.com",
+	messagingSenderId: "469335991334",
+	appId: "1:469335991334:web:8d70cd0946f880d3f632e7",
+	measurementId: "G-7NF12CYLQC"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+all_text = [];
+
+function copyDB(){
+	firebase.database().ref('text/').once('value', function(snapshot){
+		if(snapshot.val() === null){
+			all_text = [];
+		}
+		else{
+			all_text = snapshot.val();
+		}
+	})
+}
+
+
+
 $(document).ready(function(){
+	copyDB();
 
 	$('.tabs li').click(function(){
 		var tab_id = $(this).attr('data-tab');
