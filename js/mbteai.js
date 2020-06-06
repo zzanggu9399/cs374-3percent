8765273d866d5443155dda932258834da7605f13
Taeyun Woo <mswoo64@kaist.ac.kr>
2020-06-06 17:34:14 +0900

    change radio button design, change layout

--- js/mbteai.js
+++ js/mbteai.js
@@ -1,68 +1,72 @@
 //global variable
 var index; //for test page
 
-
-
 $(document).ready(function(){
     $('#before i').hide();
+    $('#prev').hide();
     index = 1;
     $('#2table').hide();
-    he = $('#MBTEAi').height();
-    $('#before').height(he);
-    $('#after').height(he);
-
 })
 
-$('#after').click(function(){
-    $('#'+index+'table').hide();
-    index++;
-    $('#page_no').html(index+"/2");
-    $('#'+index+'table').show();
-    if(index != 1){
-        $('#before i').show();
-    } 
-    if(index == 2){
-        $('#after i').hide();
-    }
-})
-
-$('#before').click(function(){
-    $('#'+index+'table').hide();
+$('#prev').click(function(){
+    $('#2table').hide();
     index--;
-    $('#page_no').html(index+"/2");
-    $('#'+index+'table').show();
-    if(index == 1){
-        $('#before i').hide();
-    } 
-    if(index != 2){
-        $('#after i').show();
-    }
+    $('#page_no').html("1/2");
+    $('#1table').show();
+    $('#prev').hide();
+    $(location).removeAttr("href");
 })
 
 $('.MBTEAi_table td').on('click', function(){
-    var k = $(this).children();
+    var k = $(this).children('input');
     if(k != null){
-        k = k.children();
+        //k = k.children('input');
+        //console.log(k);
         var n = k.attr('name');
         var v = k.attr('value');
         $("input:radio[name=" + n +"]:radio[value=" + v + "]").prop("checked", true);
+        if(v == 1){
+        $("input:radio[name=" + n +"]:radio[value=1]").parent('td').css("background", "red");
+        $("input:radio[name=" + n +"]:radio[value=2]").parent('td').css("background","#a5d194");
+        }
+        else{
+            $("input:radio[name=" + n +"]:radio[value=2]").parent('td').css("background", "red");
+            $("input:radio[name=" + n +"]:radio[value=1]").parent('td').css("background","#a5d194");
+        }
     }
 })
 
-$('#submit').click(function(){
-    var l = []
-    var branch = true;
-    for(var i = 1 ; i<21 ; i++){
-        if(! $('input:radio[name=q'+i+']').is(':checked')){// if not checked
-            branch = false;
-            l.push(i+" ");
-        }
-    }
-    if(branch){
-        
+$('#submit').click(function(){ 
+    if(index == 1){
+        index++;
+        $('#1table').hide();
+        $('#2table').show();
+        $('#prev').show();
+        $('#submit').html("SUBMIT");
+        $('#page_no').html("2/2");
+
     }
     else{
-        alert('You have not answered Question '+l+' yet');
+        var l = []
+          var branch = true;
+        for(var i = 1 ; i<21 ; i++){
+            if(! $('input:radio[name=q'+i+']').is(':checked')){// if not checked
+                branch = false;
+                l.push(i+" ");
+            }
+        }
+        $(location).attr("href",'mbteai_result.html');
+        
+
+        /*
+        if(branch){
+            
+        }
+        else{
+            alert('You have not answered Question '+l+' yet');
+        }
+        */
     }
+
 
 })
