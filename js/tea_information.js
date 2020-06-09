3e9234e6a471b42e0315e0711efbb8833e8e1f57
sonjbin <sonjbin@kaist.ac.kr>
2020-06-09 12:49:54 +0900

    tea info

--- js/tea_information.js
+++ js/tea_information.js
@@ -8,56 +8,56 @@ tea_information = [
     {
         name: "Black tea",
         src: "https://cdn.pixabay.com/photo/2016/10/26/00/59/tea-1770415_1280.jpg",
-        tag: ['rest'],
+        tag: ['sweet','rest'],
         info: "Flavor: Sweet\n\nSituation: Rest\n\nEffect: -"
     },
     {
         name: "Barley tea",
         src: "https://t1.daumcdn.net/cfile/tistory/99D11C335989C41B16",
         tag: ['savory'],
-        info: "It is good tea"
+        info: "Flavor: Savory\n\nSituation: -\n\nEffect: -"
     },
     {
         name: "Lemon tea",
         src: "https://cdn.pixabay.com/photo/2015/09/12/20/19/lemon-tea-937245_1280.jpg",
         tag: ['sour', 'vitamin c'],
-        info: "It is good tea"
+        info: "Flavor: Sour\n\nSituation: -\n\nEffect: Vitamin c"
     },
     {
         name: "Persimmon leaf tea",
         src: "https://post-phinf.pstatic.net/MjAxNzEyMTJfNiAg/MDAxNTEzMDU3MTY5NjA1.EiYvqlkIvd-aWv7uHSXO4n9rEmBebRfVRgsZf_tli6og.IWp07H7UAg3c8vKQ0ygspMJbraAjXGv5ZHwA2eH17fIg.JPEG/image_3337162861513057103005.jpg?type=w1200",
-        tag: ['vitamin c','sweat', 'savory','cold'],
-        info: "It is good tea"
+        tag: ['vitamin c','sweet', 'savory','cold'],
+        info: "Flavor: Sweet, Savory\n\nSituation: -\n\nEffect: Cold, Vitamin c"
     },
     {
         name: "Balloonflower tea",
         src: "https://sm-img3.wemep.co.kr/resize_460x460/seller/deal/1/066/3350661/09c9317b15f740629d2e2aa55a5b10c7.jpg",
         tag: ['cold','bitter'],
-        info: ""
+        info: "Flavor: Bitter\n\nSituation: -\n\nEffect: Cold"
     },
     {
         name: "Quince tea",
         src: "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F998B1E3359BB525707",
         tag: ['indigestion','sour','sweet'],
-        info: ""
+        info: "Flavor: Sour, Sweet\n\nSituation: -\n\nEffect: Indigestion"
     },
     {
         name: "Omija tea",
         src: "https://t1.daumcdn.net/cfile/tistory/990D704A5E1AE7E013",
-        tag: ['angry','stressed','sweat','sour','concentration'],
-        info: ""
+        tag: ['angry','stressed','sweet','sour','concentration'],
+        info: "Flavor: Sweet, Sour\n\nSituation: Angry, Stressed\n\nEffect: Concentration"
     },
     {
         name: "Jasmine tea",
         src: "https://previews.123rf.com/images/lianem/lianem0908/lianem090800074/11302971-%EC%9E%90%EC%8A%A4%EB%AF%BC-%EC%B0%A8.jpg",
-        tag: ['stressed','sweat','bitter'],
-        info: ""
+        tag: ['stressed','sweet','bitter'],
+        info: "Flavor: Bitter, Sweet\n\nSituation: Stressed\n\nEffect: -"
     },
     {
         name: "Chamomile tea",
         src: "https://img.mimint.co.kr/story/bbs/2019/7/16/20190716192639_bevjhamj.JPG",
         tag: ['indigestion','stressed','insomnia','herb','fruit','bitter'],
-        info: ""
+        info: "Flavor: Bitter, Fruit, Herb\n\nSituation: Stressed\n\nEffect: Indigestion, Insomnia"
     },
 
 ]
